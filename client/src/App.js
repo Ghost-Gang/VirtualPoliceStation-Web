e1673bc8bcbee0d3aee0a3b2a9e83e76fc5e88cf
atmanad <atmanad0001@gmail.com>
2021-04-10 17:53:47 +0530

    404, Unauthorized redirects added

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,35 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/style.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/style.css";
 import firebase from "firebase/app";
-// import 'firebase/auth';
 import "firebase/auth";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 
-import { ToastProvider } from 'react-toast-notifications'
+import { ToastProvider } from "react-toast-notifications";
 
-import Index from './components/Index'
-import Home from './components/Home'
-import NewComplaint from './components/NewComplaint'
-import VpsChat from './components/VpsChat'
-import ComplaintStatus from './components/ComplaintStatus'
-import SignUp from './components/Signup'
-import SignIn from './components/Login'
-import Footer from './components/Footer'
-import NavBar from './components/NavBar';
-import Admin from './components/Admin';
-import CharCert from './components/CharCert';
-import ApplyNoc from './components/ApplyNoc';
-import Profile from './components/Profile';
-import PastIncidents from './components/PastIncidents';
-
+import Index from "./components/Index";
+import Home from "./components/Home";
+import NewComplaint from "./components/NewComplaint";
+import VpsChat from "./components/VpsChat";
+import ComplaintStatus from "./components/ComplaintStatus";
+import SignUp from "./components/Signup";
+import SignIn from "./components/Login";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Admin from "./components/Admin";
+import CharCert from "./components/CharCert";
+import ApplyNoc from "./components/ApplyNoc";
+import Profile from "./components/Profile";
+import PastIncidents from "./components/PastIncidents";
+import NotFound from "./components/notfound";
+import Unauthorized from "./components/unauthorized";
 
 // initialize firebase
 const config = {
@@ -35,48 +40,99 @@ const config = {
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId
-}
+  measurementId: process.env.REACT_APP_measurementId,
+};
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
 }
 
 const App = (props) => {
-  const[user,setUser] = useState('');
-  console.log('App.js called');
-  firebase.auth().onAuthStateChanged(user => {
-    if(user){
+  const [user, setUser] = useState("");
+  const [isAuth, setIsAuth] = useState("true");
+  console.log("App.js called");
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
       setUser(user);
-    }
-  });
-
-  return (
-    <Router>
-      <NavBar />
-      <Switch>
+      if (user) setIsAuth("true");
+      else setIsAuth("false");
+    });
+  }, []);
+  console.log(isAuth);
+  if (isAuth === "true") {
+    return (
+      <Router>
+        <NavBar />
+        <Switch>
+          <ToastProvider>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/sign-up" component={SignUp} />
+            <Route exact path="/sign-in" component={SignIn} />
+            <Route exact path="/u/:uid/home" component={Home} />
+            <Route exact path="/u/:uid/my-profile" component={Profile} />
+            <Route
+              exact
+              path="/u/:uid/new-complaint"
+              component={NewComplaint}
+            />
+            <Route exact path="/u/:uid/complaint-status">
+              <ComplaintStatus user={user} />
+            </Route>
+            <Route
+              exact
+              path="/u/:uid/past-incidents"
+              component={PastIncidents}
+            />
+            <Route exact path="/u/:uid/apply-noc" component={ApplyNoc} />
+            <Route exact path="/u/:uid/vps-chat" component={VpsChat} />
+            <Route exact path="/u/:uid/admin" component={Admin} />
+            <Route
+              exact
+              path="/u/:uid/character-certificate"
+              component={CharCert}
+            />
+          </ToastProvider>
+        </Switch>
+        <Footer />
+      </Router>
+    );
+  } else if (isAuth === "false") {
+    return (
+      <Router>
+        <NavBar />
         <ToastProvider>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/sign-up" component={SignUp} />
-          <Route exact path="/sign-in" component={SignIn} />
-          <Route exact path="/u/:uid/home" component={Home} />
-          <Route exact path="/u/:uid/my-profile" component={Profile} />
-          <Route exact path="/u/:uid/new-complaint" component={NewComplaint} />
-          <Route exact path="/u/:uid/complaint-status"><ComplaintStatus user={user}/></Route>
-          <Route exact path="/u/:uid/past-incidents" component={PastIncidents} />
-          <Route exact path="/u/:uid/apply-noc" component={ApplyNoc} />
-          <Route exact path="/u/:uid/vps-chat" component={VpsChat} />
-          <Route exact path="/u/:uid/admin" component={Admin} />
-          <Route exact path="/u/:uid/character-certificate" component={CharCert} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/sign-in" component={SignIn} />
+            <Route exact path="/sign-up" component={SignUp} />
+            <Route
+              exact
+              path="/u/:uid/new-complaint"
+              component={Unauthorized}
+            />
+            <Route
+              exact
+              path="/u/:uid/complaint-status"
+              component={Unauthorized}
+            />
+            <Route
+              exact
+              path="/u/:uid/past-incidents"
+              component={Unauthorized}
+            />
+            <Route exact path="/u/:uid/apply-noc" component={Unauthorized} />
+            <Route exact path="/u/:uid/vps-chat" component={Unauthorized} />
+            <Route
+              exact
+              path="/u/:uid/character-certificate"
+              component={Unauthorized}
+            />
+            <Route component={NotFound} />
+          </Switch>
         </ToastProvider>
-        <Route
-          render={function () {
-            return <h1>Not Found</h1>;
-          }}
-        />
-      </Switch>
-      <Footer />
-    </Router >
-  );
-}
+        <Footer />
+      </Router>
+    );
+  }
+};
 
 export default App;
---
+++ client/src/components/notfound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../css/notfound.css"
+
+function notfound() {
+    return (
+        <div id="message">
+            <h2>404</h2>
+            <h1>Page Not Found</h1>
+            <p>The specified file was not found on this website. Please check the URL for mistakes and try again.</p>
+        </div>
+    )
+}
+
+export default notfound
